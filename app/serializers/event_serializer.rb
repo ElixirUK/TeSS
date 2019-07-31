4918c1651524fe271afb54a67605f753b9a7e65d
Niall Beard <niallbeard@gmail.com>
2019-07-31 13:57:52 +0100

    add prerequisites and learning outcomes to JSON API

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -3,7 +3,8 @@ class EventSerializer < ApplicationSerializer
              :start, :end, :sponsors, :venue, :city, :county, :country, :postcode,
              :latitude, :longitude, :source, :slug, :online, :cost, :for_profit, :last_scraped, :scraper_record,
              :keywords, :event_types, :target_audience, :capacity, :eligibility, :contact, :host_institutions,
-             :scientific_topics, :operations, :external_resources, :created_at, :updated_at
+             :scientific_topics, :operations, :external_resources, :created_at, :updated_at, :prerequisites,
+             :learning_outcomes
 
   attribute :report, if: -> { policy(object).view_report? }
 
